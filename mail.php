14cd6ca8c4674ab1b4e1e2d2cdf24a459c5f59be
Gabor Soltesz-Keri <61150302+sollabong@users.noreply.github.com>
2021-04-07 17:40:47 +0200

    Add files via upload

--- mail.php
+++ mail.php
@@ -3,9 +3,10 @@ $email = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 $formcontent="Feladó: $name \n Üzenet: $message \n Telefon: $phone";
-$recipient = "solteszgabb@gmail.com";
+$recipient = "fanni1virag@gmail.com";
 $subject = "Megkeresés a stelliumconsulting.hu oldalról";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Hoppá, valami hiba történt!");
-echo "Köszönöm!";
+echo "<script>alert('Köszönöm, hamarosan válaszolok');</script>";
+echo "<script type='text/javascript'>window.location.href='index.html'</script>";
 ?>
